140c2c37b8acac874305dce91d064b02c2392c6c
ccsahin6436-del <ccsahin6436@gmail.com>
2025-12-01 01:32:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,7 @@ const questions = [
     "2) Günlük stres seviyen nasıl?",
     "3) Uyku düzenin ne kadar yeterli?",
     "4) Günlük enerjin nasıl?",
-    "5) Kendine ayırdığın vakitten memnun musun?",
-    "6) Son 1 ayda fiziksel ağrı yaşadın mı?",
-    "7) Günlük su tüketimin yeterli mi?",
-    "8) Haftalık spor yapma sıklığın?",
-    "9) Beslenme düzenin ne kadar sağlıklı?",
-    "10) Mevsimsel hastalıklara yakalanma sıklığın?",
-    "11) Sosyal ilişkilerinden memnun musun?",
-    "12) Kendini motive hissediyor musun?",
-    "13) Gün içinde ne kadar hareket ediyorsun?",
-    "14) Kafein tüketimin nasıl?",
-    "15) Psikolojik olarak kendini güçlü hissediyor musun?",
-    "16) Nefes egzersizi veya meditasyon yapıyor musun?",
-    "17) Kendine güven seviyen nasıl?",
-    "18) İş/okul stresin ne durumda?",
-    "19) Günlük yorgunluk seviyen?",
-    "20) Sağlık kontrollerini aksatır mısın?",
-    "21) Genel fiziksel gücünü nasıl değerlendirirsin?",
-    "22) Kendine koyduğun hedefleri uygulayabiliyor musun?",
-    "23) Gün içinde odaklanma seviyen nasıl?",
-    "24) Kişisel bakım alışkanlıkların nasıl?",
+    ...
     "25) Genel sağlık durumunu nasıl değerlendirirsin?"
 ];
 
